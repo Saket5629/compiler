4e3bd1a0239ac7a027f0cd693a7d6a0f6eed9d31
Saket <saket.singh@vetic.in>
2025-11-26 00:13:47 +0530

    feat: implemented to run on mac feature

--- src/script.js
+++ src/script.js
@@ -7,9 +7,9 @@ const compiler = require('compilex');
 const port = process.env.PORT || 3000;
 const collection = require('./mongodb');
 
-const tempelatePath = path.join(__dirname,'../tempelates');
+const tempelatePath = path.join(__dirname, '../tempelates');
 
-const options = {stats:true};
+const options = { stats: true };
 compiler.init(options);
 
 // app.use((req,res,next)=>{
@@ -18,60 +18,61 @@ compiler.init(options);
 app.use(bodyP.json());
 app.use(express.static(__dirname));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"../")));
-// app.use('/codemirror', express.static(path.join(__dirname, '..', 'codemirror-5.65.16')));
+app.use(express.static(path.join(__dirname, "../")));
+// Serve CodeMirror assets from the folder that lives inside src/
+app.use('/codemirror', express.static(path.join(__dirname, 'codemirror-5.65.16')));
 app.use(express.urlencoded({ extended: false }));
-app.set('view engine','hbs');
-app.set("views",tempelatePath);
+app.set('view engine', 'hbs');
+app.set("views", tempelatePath);
 
 app.get('/', (req, res) => {
-  res.render("login");
+    res.render("login");
 });
 
-app.get('/signup',(req,res)=>{
-  res.render("signup");
+app.get('/signup', (req, res) => {
+    res.render("signup");
 })
 
-app.post('/signup',async (req,res)=>{
-  const data = {
-      name: req.body.name,
-      password: req.body.password
-  }
+app.post('/signup', async (req, res) => {
+    const data = {
+        name: req.body.name,
+        password: req.body.password
+    }
 
-  const checking = await collection.findOne({ name: req.body.name })
+    const checking = await collection.findOne({ name: req.body.name })
 
-  try{
-      if( checking && checking.name === req.body.name && checking.password===req.body.password){
-          res.send("user details already exists");
-      }
-      else{
-          await collection.insertMany([data]);
-          res.render("login");
-      }
-  }
-  catch{
-      res.send("wrong inputs")
-  }
+    try {
+        if (checking && checking.name === req.body.name && checking.password === req.body.password) {
+            res.send("user details already exists");
+        }
+        else {
+            await collection.insertMany([data]);
+            res.render("login");
+        }
+    }
+    catch {
+        res.send("wrong inputs")
+    }
 
 })
 
-app.post('/login',async (req,res)=>{
-  try{
-      const check = await collection.findOne({ name: req.body.name })
+app.post('/login', async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.name })
 
-      if(check && check.password === req.body.password){
-          res.status(201).sendFile(path.join(__dirname, '..', 'new.html'));
-      }
-      else{
-          res.send("There is no such account with this detail");
-      }
-  }
-  catch{
-      res.send("wrong inputs")
-      }
+        if (check && check.password === req.body.password) {
+            res.status(201).sendFile(path.join(__dirname, '..', 'new.html'));
+        }
+        else {
+            res.send("There is no such account with this detail");
+        }
+    }
+    catch {
+        res.send("wrong inputs")
+    }
 })
 
-app.get('/new*',(req,res)=>{
+app.get('/new*', (req, res) => {
     res.sendFile(path.join(__dirname, 'compile.html'));
 });
 
@@ -80,122 +81,90 @@ app.post("/compile", (req, res) => {
     var input = req.body.input
     var lang = req.body.lang
     try {
-
+        const platform = process.platform; // 'darwin', 'win32', 'linux'
+        const isWin = platform === 'win32';
+        let responded = false;
+        const reply = payload => {
+            if (responded) return;
+            responded = true;
+            res.send(payload);
+        };
         if (lang == "C++" || lang == "C") {
+            const envData = { OS: isWin ? "windows" : "linux", cmd: "g++", options: { timeout: 10000 } };
+            const handleResult = data => {
+                if (data.output) reply(data);
+                else {
+                    console.log(data.error);
+                    reply({ output: "error" });
+                }
+              };
             if (!input) {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
-                compiler.compileCPP(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        console.log("error")
-                        res.send({ output: "error" })
-                    }
-                });
+                compiler.compileCPP(envData, code, handleResult);
             }
             else {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
-                compiler.compileCPPWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        console.log("error")
-                        res.send({ output: "error" })
-                    }
-                });
+                compiler.compileCPPWithInput(envData, code, input, handleResult);
             }
         }
         else if (lang == "Java") {
+            const envData = { OS: isWin ? "windows" : "linux", cmd: "javac", options: { timeout: 10000 } };
+            const handleResult = data => {
+                if (data.output) reply(data);
+                else {
+                    console.log(data.error);
+                    reply({ output: "error" });
+                }
+              };
             if (!input) {
-                var envData = { OS: "windows" };
-                compiler.compileJava(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        console.log("error")
-                        res.send({ output: "error" })
-                    }
-                })
+                compiler.compileJava(envData, code, handleResult);
             }
-            else { 
-                var envData = { OS: "windows" };
-                compiler.compileJavaWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        console.log("error")
-                        res.send({ output: "error" })
-                    }
-                })
+            else {
+                compiler.compileJavaWithInput(envData, code, input, handleResult);
             }
         }
         else if (lang == "Python") {
+            const envData = { OS: isWin ? "windows" : "linux", cmd: isWin ? "python" : "python3", options: { timeout: 10000 } };
+            console.log(envData);
+            const handleResult = data => {
+                if (data.output) reply(data);
+                else {
+                    console.log(data.error);
+                    reply({ output: "error" });
+                }
+            };
             if (!input) {
-                var envData = { OS: "windows" };
-                compiler.compilePython(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        console.log("error")
-                        res.send({ output: "error" })
-                    }
-                });
+                compiler.compilePython(envData, code, handleResult);
             }
             else {
-                var envData = { OS: "windows" };
-                compiler.compilePythonWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        console.log("error")
-                        res.send({ output: "error" })
-                    }
-                });
+                compiler.compilePythonWithInput(envData, code, input, handleResult);
             }
         }
         else if (lang == "CS") {
+            const envData = { OS: isWin ? "windows" : "linux", cmd: "csc", options: { timeout: 10000 } };
+            const handleResult = data => {
+                if (data.output) reply(data);
+                else {
+                    console.log(data.error);
+                    reply({ output: "error" });
+                }
+            };
             if (!input) {
-                var envData = { OS: "windows" };
-                compiler.compileCS(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        console.log("error")
-                        res.send({ output: "error" })
-                    }
-                });
+                compiler.compileCS(envData, code, handleResult);
             }
             else {
-                var envData = { OS: "windows" };
-                compiler.compileCSWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        console.log("error")
-                        res.send({ output: "error" })
-                    }
-                });
+                compiler.compileCSWithInput(envData, code, input, handleResult);
             }
         }
     }
     catch (e) {
-        console.log("error")
+        console.log(e)
     }
 })
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
-compiler.flush(function() {
+compiler.flush(function () {
     console.log("All temporary files flushed !");
 });
